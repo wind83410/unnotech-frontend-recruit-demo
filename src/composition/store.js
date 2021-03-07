a66325b545127b97fe45e8bdaac3937f9c401d46
Mack Chen <wind83410@gmail.com>
2021-03-07 20:24:50 +0800

    新增表格等待畫面

--- src/composition/store.js
+++ src/composition/store.js
@@ -1,7 +1,7 @@
-import { reactive, toRaw, computed } from 'vue'
+import { ref, reactive, toRaw, computed } from 'vue'
 
 export const books = reactive([]) // 書的圖片和標題
-export const regBook = reactive({ // 編輯表格用
+export const regBook = reactive({
   id: '',
   price: 0,
   count: 0
@@ -11,6 +11,9 @@ export const bookInFocus = computed(() => {
   const theBook = books.find(book => book.id === regBook.id)
   return theBook ? theBook : {}
 })
+
+// 狀態四種值：init, error, working, done
+export const workState = ref('init')
 
 // 書名和圖片
 export const getBooks = () => {
@@ -30,6 +33,7 @@ export const getBooks = () => {
 
 // 取得書的基本資料
 export const getBookProfile = id => {
+  workState.value = 'working'
   fetch(`https://fe-interview-api.unnotech.com/profile/${id}`, {
     headers: {
       'content-type': 'application/json'
@@ -39,17 +43,20 @@ export const getBookProfile = id => {
     if (response.ok) {
       return response.json()
     } else {
+      workState.value = 'error'
       return {}
     }
   }).then(data => {
     for (let prop in regBook) {
       regBook[prop] = data[prop]
     }
+    workState.value = 'done'
   })
 }
 
 // 更改書的基本資料
 export const setBookProperties = () => {
+  workState.value = 'working'
   const { price, count } = toRaw(regBook)
   fetch(`https://fe-interview-api.unnotech.com/profile/${regBook.id}`, {
     headers: {
@@ -59,7 +66,11 @@ export const setBookProperties = () => {
     body: JSON.stringify({ price, count })
   }).then(response => {
     if (response.ok) {
-      console.log('succeeded to modify')
+      workState.value = 'done'
+      alert('修改成功')
+    } else {
+      workState.value = 'error'
+      alert('修改失敗')
     }
   }).catch(Error('Failed to modify'))
 }
